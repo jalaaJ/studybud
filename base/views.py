1c1b8ae4d4b9a7dbfc4483bac8a76326c537651b
JalaaJ <134323854+jalaaJ@users.noreply.github.com>
2023-11-03 21:49:26 +0200

    Added clickable room names and and added the routing to the room according to its id

--- base/views.py
+++ base/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 
+rooms = [
+    {"id": 1, "name": "Let's learn Python!"},
+    {"id": 2, "name": "Design with me"},
+    {"id": 3, "name": "Frontend developers"},
+]
+
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    context = {"rooms": rooms}
+    return render(request, "base/home.html", context)
 
 
-def room(request):
-    return render(request, "room.html")
+def room(request, pk):
+    room = None
+    for each_room in rooms:
+        if each_room["id"] == int(pk):
+            room = each_room
+    context = {"room": room}
+    return render(request, "base/room.html", context)

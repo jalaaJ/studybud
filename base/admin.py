b2652daf09edeee661a43c93366e06defe41f556
JalaaJ <134323854+jalaaJ@users.noreply.github.com>
2023-11-05 12:45:10 +0200

    Created the Topic and the Message models, added the topic and host relation to the Room model and applied the migrations

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Room
+from .models import Room, Topic, Message
 
 # We need to register the model in order to be able to see it in the admin panel
 admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Message)
--- base/models.py
+++ base/models.py
@@ -2,11 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Room(models.Model):
-    # host =
-
-    # topic =
-
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
 

10cb0de766ad6788c7d91d5fe7bd635dcab734e9
JalaaJ <134323854+jalaaJ@users.noreply.github.com>
2023-11-05 11:40:06 +0200

    Minor change

--- base/models.py
+++ base/models.py
@@ -1,1 +1,22 @@
 from django.db import models
+
+
+class Room(models.Model):
+    # host =
+
+    # topic =
+
+    name = models.CharField(max_length=100)
+
+    description = models.TextField(null=True, blank=True)
+
+    # participants =
+
+    # takes a timestamp everytime that we save
+    updated = models.DateTimeField(auto_now=True)
+
+    # takes a timestamp when we create the room
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name

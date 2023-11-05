68ce610c946f9db65e0af5bd7fd8322be00668d6
JalaaJ <134323854+jalaaJ@users.noreply.github.com>
2023-11-05 12:33:33 +0200

    Created the Message table or model, with a user and a room relation

--- base/models.py
+++ base/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Room(models.Model):
@@ -7,7 +8,6 @@ class Room(models.Model):
     # topic =
 
     name = models.CharField(max_length=100)
-
     description = models.TextField(null=True, blank=True)
 
     # participants =
@@ -20,3 +20,14 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]

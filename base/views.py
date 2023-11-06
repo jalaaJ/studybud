8345fd9a6174a1fd809bf56551a282d0fdefa1f9
JalaaJ <134323854+jalaaJ@users.noreply.github.com>
2023-11-06 16:32:50 +0200

    Added the RoomForm class which a ModelForm to the form.py along with the metadata that contains the Room and all its fields then rendered the form

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Room
+from .forms import RoomForm
 
 # rooms = [
 #     {"id": 1, "name": "Let's learn Python!"},
@@ -22,5 +23,6 @@ def room(request, pk):
 
 
 def createRoom(request):
-    context = {}
+    form = RoomForm()
+    context = {"form": form}
     return render(request, "base/room_form.html", context)

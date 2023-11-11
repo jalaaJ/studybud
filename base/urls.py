47198208e4c369025958e8874a2ad198d9fe25dc
JalaaJ <134323854+jalaaJ@users.noreply.github.com>
2023-11-11 13:41:50 +0200

    nothing really

--- base/urls.py
+++ base/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path("login/", views.loginPage, name="login"),
     path("logout/", views.logoutUser, name="logout"),
     path("register/", views.registerUser, name="register"),
-    path("", views.home, name="home"),
     path("room/<str:pk>", views.room, name="room"),
     path("profile/<str:pk>", views.userProfile, name="user-profile"),
     path("create-room/", views.createRoom, name="create-room"),

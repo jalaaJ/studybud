49b39cc91aa403eb5055b3b0f814d85ff00dd379
JalaaJ <134323854+jalaaJ@users.noreply.github.com>
2023-11-16 17:28:32 +0200

    Finished the simple REST api for the clients

--- base/api/views.py
+++ base/api/views.py
@@ -1,7 +1,27 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from base.models import Room
+from .serializers import RoomSerializer
 
 
 # Allow clients to access the rooms through these apis
+@api_view(["GET"])
 def getRoutes(request):
     routes = ["GET /api", "GET /api/rooms", "GET /api/rooms/:id"]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+
+@api_view(["GET"])
+def getRooms(request):
+    rooms = Room.objects.all()
+
+    # many=True becuase we're returning more than one value
+    serilizer = RoomSerializer(rooms, many=True)
+    return Response(serilizer.data)
+
+
+@api_view(["GET"])
+def getRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    serilizer = RoomSerializer(room, many=False)
+    return Response(serilizer.data)
